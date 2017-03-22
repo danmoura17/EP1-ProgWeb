026a8317f0b381c6a3532b3eb108d3c555d6372f
Daniel <danmoura17@gmail.com>
2017-03-21 22:58:56 -0300

    Adding Read Funcion

--- cliente/admin.py
+++ cliente/admin.py
@@ -6,6 +6,8 @@ class ClienteAdmin(admin.ModelAdmin):
 	model = Cliente 
 	date_hierarchy = 'data_nascimento'
 	list_display = ('nome', 'data_nascimento', 'salario', 'email', 'filhos')
-	list_filter = ('nome', 'data_nascimento', 'salario', 'email', 'filhos')
+	search_fields = ('nome', 'data_nascimento', 'salario', 'email', 'filhos')
+
+
 admin.site.register(Cliente, ClienteAdmin)
 # Register your models here.
--- cliente/views.py
+++ cliente/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cliente.models import Cliente
 
 def home(request):
 	return HttpResponse('Hello World!')
@@ -7,5 +8,9 @@ def home(request):
 
 def cliente(request):
 	data = {}
+	data['lista_clientes'] = Cliente.objects.all()
 	data['djangomoc'] = 'DjangoMOC - Curso presencial de Python e Django' 
 	return render(request, 'clientes.html', data)
+
+def cliente_update(request, pk):
+	return HttpResponse('Cliente '+ str(pk))
